d50ccd1468a98a5dcc1d7a911668a1ea6d81ca9b
Juan Pablo Zarza Garcia <147415904+Zarzajpg@users.noreply.github.com>
2026-08-28 14:18:15 -0300

    Create Arregloburbuja.java

---
+++ Clase3/Arregloburbuja.java
@@ -0,0 +1,19 @@
+package Clase3;
+import java.util.Arrays;
+
+public class Arregloburbuja {
+
+	public static void main(String[] args) {
+		int[] numeros = {3, 5, 9, 2, 1, 6, 0, 7};
+		for (int i = 0; i < numeros.length - 1; i++) {
+			for (int j = 0; j < numeros.length - 1 - i; j++) {
+				if (numeros[j] > numeros[j + 1]) {
+					int aux = numeros[j];
+					numeros[j] = numeros[j + 1];
+					numeros[j + 1] = aux;
+				}
+			}
+		}
+		System.out.println(Arrays.toString(numeros));
+	}
+}

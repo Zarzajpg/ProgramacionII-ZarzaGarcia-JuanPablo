2f9ea23b511143e05d0e63abc7ea6725bb49c19c
Juan Pablo Zarza Garcia <147415904+Zarzajpg@users.noreply.github.com>
2026-08-28 14:42:14 -0300

    Update Arregloburbuja.java

--- Clase3/Arregloburbuja.java
+++ Clase3/Arregloburbuja.java
@@ -6,7 +6,9 @@ public class Arregloburbuja {
 	public static void main(String[] args) {
 		int[] numeros = {3, 5, 9, 2, 1, 6, 0, 7};
 		for (int i = 0; i < numeros.length - 1; i++) {
-			for (int j = 0; j < numeros.length - 1 - i; j++) {
+			for (int j = 0; j < numeros.length - 1  - i; j++) {
+				// Debug
+				System.out.println(i);
 				if (numeros[j] > numeros[j + 1]) {
 					int aux = numeros[j];
 					numeros[j] = numeros[j + 1];
